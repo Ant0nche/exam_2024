d13134ea8d7f89f8dab788b8be118e5e05f018c8
Ant0nche <113699926+Ant0nche@users.noreply.github.com>
2024-01-12 17:13:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let searchedGuides;
 
 /*Формирует URL для API запросов.*/
 function getURL(path) {
-    let url = new URL(`http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/${path}`);
+    let url = new URL(`https://edu.std-900.ist.mospolytech.ru/api/${path}`);
     url.searchParams.set("api_key", "51f99d95-e6b9-481d-a4fa-b02fba54a3c7");
     return url;
 }
